b16c523ce79e3d638dccdafafcf14e8711e2e55f
gblin1 <guiblin97@gmail.com>
2022-04-09 10:01:10 +0200

    Trial to implement 5.1

--- src/03_drivers/05_01_char_driver_sysfs/char_driver_sysfs.c
+++ src/03_drivers/05_01_char_driver_sysfs/char_driver_sysfs.c
@@ -3,6 +3,10 @@
 #include <linux/kernel.h> // needed for debugging
 #include <linux/device.h> /* needed for sysfs handling */
 #include <linux/platform_device.h>
+#include <linux/cdev.h>
+#include <linux/miscdevice.h>
+
+#define CHAR_BUFFER_SIZE 1000
 
 struct skeleton_data {
     int id;
@@ -10,6 +14,11 @@ struct skeleton_data {
 };
 
 static struct skeleton_data data;
+
+// Char device
+static struct cdev driver_cdev;
+
+static dev_t driver_dev;
 
 ssize_t sysfs_show_data(struct device* dev, struct device_attribute* attr, char* buf)
 {
@@ -27,44 +36,142 @@ ssize_t sysfs_store_data(struct device* dev, struct device_attribute* attr,const
 DEVICE_ATTR(Data, 0664, sysfs_show_data, sysfs_store_data);
 
 // static struct class* sysfs_class;
-// static struct device* sysfs_device;
+static struct device* sysfs_device;
 
-static struct platform_device sysfs_device = {
-    .name       = "sysfs_module",
-    .id         = -1,
-    .dev.release= sysfs_dev_release,
+static int driver_open(struct inode* i, struct file* f)
+{
+    printk("driver_open: major %d, minor %d\n", imajor(i), iminor(i));
+    // Opening with appending intent
+    if ((f->f_flags & (O_APPEND)) != 0)
+    {
+        printk("driver : opened for appending data...\n");
+    }
+    // Opening with reading and writing intent
+    else if ((f->f_mode & (FMODE_READ | FMODE_WRITE)) != 0) 
+    {
+        printk("driver : opened for reading & writing...\n");
+    } 
+    // Opening with reading intent
+    else if ((f->f_mode & FMODE_READ) != 0) 
+    {
+        printk("driver : opened for reading...\n");
+    } 
+    // Opening wiht writing intent
+    else if ((f->f_mode & FMODE_WRITE) != 0) 
+    {
+        printk("driver : opened for writing...\n");
+    }
+    return 0;
+}
+
+// Reading contents of device file
+static ssize_t driver_read(struct file* f, char* __user buf, size_t count, loff_t* off)
+{
+    // Remaining space in buffer left after write operation
+    ssize_t remaining = CHAR_BUFFER_SIZE - (ssize_t)(*off);
+    // Apply offset
+    char* buf_ptr = (char*)f->private_data + *off;
+
+    int cpy_status = 0;
+    // Is enough space available for storing further bytes
+    if (count >= remaining)
+    {
+        count = remaining;
+    }
+    // Compute new offset
+    *off += count;
+    // Copy from storage buffer to output buffer
+    cpy_status = copy_to_user(buf, buf_ptr, count);
+    if (cpy_status != 0)
+    {
+        count = -EFAULT;
+    } 
+
+    printk("driver_read\n");
+    return count;
+}
+
+// Writing contents into device file
+static ssize_t driver_write(struct file* f, const char* __user buf, size_t count, loff_t* off)
+{
+    int cpy_status = 0;
+
+    // Remaining space in buffer left after write operation
+    ssize_t remaining = CHAR_BUFFER_SIZE - (ssize_t)(*off);
+    printk("driver_write function\n");
+
+    // Is enough space available for storing further bytes
+    if (count >= remaining)
+    {
+        count = -EIO;
+    }
+
+    // Save/store further entered bytes in the buffer
+    if (count > 0)
+    {
+        char* buf_ptr = (char*)f->private_data + *off;
+        // Compute new offset
+        *off += count;
+        buf_ptr[count] = 0; // null terminated
+        // Copy from input buffer to storage buffer
+        cpy_status = copy_from_user(buf_ptr, buf, count);
+        if(cpy_status)
+        {
+            count = -EFAULT;
+        }
+    }
+    return count;
+}
+
+// Closing of device file
+static int driver_release(struct inode *i, struct file *f)
+{
+    printk("driver_release \n");
+    return 0;
+}
+
+static struct file_operations sysfs_fops = {
+    .owner = THIS_MODULE,
+    .open = driver_open,
+    .read = driver_read,
+    .write = driver_write,
+    .release = driver_release,
 };
+
+static struct miscdevice misc_sysfs_device = {
+    .minor = MISC_DYNAMIC_MINOR, // dynamic minor attribution
+    .name = "misc_sysfs_device",
+    .mode = 0, // rights under /dev dir
+    .fops = &sysfs_fops, // returns to regular file_operations struct
+};
+
+
 
 static int __init skeleton_init(void)
 {
-    int status = 0;
+    // Dynamic allocation of driver numbers to avoid conflicts with other devices
+    alloc_chrdev_region(&driver_dev, 0, 1, "driver_module");
 
-    if(status == 0)
-    {
-        status = platform_device_register(&sysfs_device);
-    }
-    if(status == 0)
-    {
-        status = device_create_file(&sysfs_device.dev, &dev_attr_Data);
-    }
+    // Char device initialization
+    cdev_init(&driver_cdev, &sysfs_fops);
 
-    // sysfs_class = class_create(THIS_MODULE, "my_sysfs_class");
-    // sysfs_device = device_create(sysfs_class, NULL, 0, NULL, "my_sysfs_device");
+    // Save driver to kernel
+    cdev_add(&driver_cdev, driver_dev, 1);
 
-    // device_create_file(sysfs_device, &dev_attr_Data);
+    // Create sysfs file
+    misc_register(&misc_sysfs_device);
+    device_create_file(sysfs_device, &dev_attr_Data);
 
-    pr_info("Linux module skeleton loaded\n");
-    return status;
+    pr_info("Linux driver module sysfs loaded\n");
+    return 0;
 }
 
 static void __exit skeleton_exit(void)
 {
-
-    device_remove_file(&sysfs_device.dev, &dev_attr_Data);
-    // device_destroy(sysfs_class, 0);
-    // class_destroy(sysfs_class);
-    platform_device_unregister(&sysfs_device);
-
+    device_remove_file(sysfs_device, &dev_attr_Data);
+    misc_deregister(&misc_sysfs_device);
+    cdev_del(&driver_cdev);
+    unregister_chrdev_region(driver_dev, 1);
     pr_info("Linux module skeleton unloaded\n");
 }
 
