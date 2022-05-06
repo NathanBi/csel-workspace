e6e85286f3c69588c13e0ccabcb41d5061237d9e
guillaum.blin <guillaume.blin@master.hes-so.ch>
2022-05-06 09:34:54 +0000

    LED blink based on timerfd

--- src/04_system/01_silly_led_control.c
+++ src/04_system/01_silly_led_control.c
@@ -33,6 +33,7 @@
 #include <time.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 /*
  * status led - gpioa.10 --> gpio10
@@ -71,20 +72,25 @@ int main(int argc, char* argv[])
 {
     // timer creation
     int timerfd, epfd, ret;
+    uint64_t res;
     struct epoll_event ev;
     struct itimerspec ts;
-    int msec = 5000;
-    uint64_t res;
+    int msec = 3000;
+
     timerfd = timerfd_create(CLOCK_REALTIME, 0);
     printf("created timerfd %d\n", timerfd);
     printf("Starting at (%d)...\n", (int)time(NULL));
 
     // interval for periodic timer
-    ts.it_interval.tv_sec = 0;
+    ts.it_interval.tv_sec = msec/1000;
     ts.it_interval.tv_nsec = 0;
     // expiration
-    ts.it_value.tv_sec = msec / 1000;
-	ts.it_value.tv_nsec = (msec % 1000) * 1000000;
+    ts.it_value.tv_sec = msec/1000;
+	ts.it_value.tv_nsec = 0;
+
+    int led = open_led();
+    bool toggle = true;
+    pwrite(led, "1", sizeof("1"), 0);
 
     if (timerfd_settime(timerfd, 0, &ts, NULL) < 0) {
 		printf("timerfd_settime() failed: errno=%d\n", errno);
@@ -93,13 +99,15 @@ int main(int argc, char* argv[])
 	}
 	printf("set timerfd time=%s\n", itimerspec_dump(&ts));
 
-	epfd = epoll_create(1);
+    // create the context
+	epfd = epoll_create1(0);
 	if (epfd == -1) {
 		printf("epoll_create() failed: errno=%d\n", errno);
 		close(timerfd);
 		return EXIT_FAILURE;
 	}
 
+    //syscall for context control
     ev.events = EPOLLIN;
 	if (epoll_ctl(epfd, EPOLL_CTL_ADD, timerfd, &ev) == -1) {
 		printf("epoll_ctl(ADD) failed: errno=%d\n", errno);
@@ -109,70 +117,34 @@ int main(int argc, char* argv[])
 	}
 	printf("added timerfd to epoll set\n");
 
-    sleep(1);
+    while(1)
+    {
+        memset(&ev, 0, sizeof(ev));
+	    ret = epoll_wait(epfd, &ev, 1000, 10000); // wait up to 500ms for timer
+	    if (ret < 0) {
+            printf("epoll_wait() failed: errno=%d\n", errno);
+            close(epfd);
+            close(timerfd);
+            return EXIT_FAILURE;
+	    }
+	    printf("waited on epoll, ret=%d\n", ret);
+        printf("event=%ld on fd=%d\n", ev.events, ev.data.fd);
 
-    memset(&ev, 0, sizeof(ev));
-	ret = epoll_wait(epfd, &ev, 1, 500); // wait up to 500ms for timer
-	if (ret < 0) {
-		printf("epoll_wait() failed: errno=%d\n", errno);
-		close(epfd);
-		close(timerfd);
-		return EXIT_FAILURE;
-	}
-	printf("waited on epoll, ret=%d\n", ret);
-
-	ret = read(timerfd, &res, sizeof(res));
-	printf("read() returned %d\n", ret);
-
-	if (close(epfd) == -1) {
-		printf("failed to close epollfd: errno=%d\n", errno);
-		return EXIT_FAILURE;
-	}
-
-	if (close(timerfd) == -1) {
-		printf("failed to close timerfd: errno=%d\n", errno);
-		return EXIT_FAILURE;
-	}
-
-	return EXIT_SUCCESS;
-
-    // long duty   = 2;     // %
-    // long period = 1000;  // ms
-    // if (argc >= 2) period = atoi(argv[1]);
-    // period *= 1000000;  // in ns
-
-    // // compute duty period...
-    // long p1 = period / 100 * duty;
-    // long p2 = period - p1;
-
-    // int led = open_led();
-    // pwrite(led, "1", sizeof("1"), 0);
-
-    // struct timespec t1;
-    // clock_gettime(CLOCK_MONOTONIC, &t1);
-
-    // int k = 0;
-    // while (1) {
-    //     struct timespec t2;
-    //     clock_gettime(CLOCK_MONOTONIC, &t2);
-
-    //     long delta =
-    //         (t2.tv_sec - t1.tv_sec) * 1000000000 + (t2.tv_nsec - t1.tv_nsec);
-
-    //     int toggle = ((k == 0) && (delta >= p1)) | ((k == 1) && (delta >= p2));
-    //     if (toggle) {
-    //         t1 = t2;
-    //         k  = (k + 1) % 2;
-    //         if (k == 0)
-    //             pwrite(led, "1", sizeof("1"), 0);
-    //         else
-    //             pwrite(led, "0", sizeof("0"), 0);
-    //     }
-    // }
-
-    // return 0;
+        ret = read(timerfd, &res, sizeof(res));
+        printf("Toggle led \n");
+        
+        if(toggle == true)
+        {
+            pwrite(led, "1", sizeof("1"), 0);
+            toggle = false;
+        }
+        else
+        {
+            pwrite(led, "0", sizeof("0"), 0);
+            toggle = true;
+        }
+    }
 }
-
 
 static char *itimerspec_dump(struct itimerspec *ts)
 {
